4d2d40df4c257062817a56aab5be34ce3930296d
laurapoc <laura.pociuniene@gmail.com>
2019-12-18 18:52:24 +0200

    Finished code and some polishing

--- assets/scripts/custom.js
+++ assets/scripts/custom.js
@@ -4,20 +4,28 @@ const url = "https://picsum.photos/v2/list";
 xhr.responseType = "json";
 xhr.onreadystatechange = () => {
   if (xhr.readyState === XMLHttpRequest.DONE) {
-    console.log(xhr.response);
     for (let i = 0; i < xhr.response.length; i++) {
       let element = document.createElement("img");
-
-      console.log(xhr.response[i].download_url.split("/"));
       let splitArr = xhr.response[i].download_url.split("/");
-      console.log(splitArr[5], splitArr[6]);
       splitArr[5] = 200;
       splitArr[6] = 200;
-      console.log(splitArr.join("/"));
       element.setAttribute("src", splitArr.join("/"));
       element.setAttribute("alt", xhr.response[i].author);
-      console.log(element);
       document.getElementById("scrollbar").appendChild(element);
+
+      //   open large image:
+      element.addEventListener("click", function() {
+        document.getElementById("full-image").setAttribute("src", xhr.response[i].download_url);
+        document.getElementById("p1").innerHTML =
+          "Author: " +
+          xhr.response[i].author +
+          ". Image width: " +
+          xhr.response[i].width +
+          " px" +
+          ", height: " +
+          xhr.response[i].height +
+          " px.";
+      });
     }
   }
 };
